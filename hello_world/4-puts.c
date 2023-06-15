9a2e0116b3037e1cee5dc75e001d283713237772
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-15 16:20:43 +0000

    fix task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(void) {
-   
-   put("Programming is like building a multilingual puzzle");
-   return 0;
+
+/**
+ * main - rogram that prints
+ * Return: always ZERO
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
