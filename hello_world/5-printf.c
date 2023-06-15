d74f6ad52d785f68d625e515dae7de5b11743fe9
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-15 16:37:51 +0000

    compile 5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - rogram that prints
+ * Return: always ZERO
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
