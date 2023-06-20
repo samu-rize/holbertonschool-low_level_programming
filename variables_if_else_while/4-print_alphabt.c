de6bad347e358bf63e34aec4d9f9fef510473b6c
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-20 11:51:59 +0000

    print alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program prints the alphabet in lowercase, followed by a new line.
+ * Return: always ZERO
+**/
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i < 123)
+	{
+		if (i == 101 || 113)
+		{
+			i++;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
