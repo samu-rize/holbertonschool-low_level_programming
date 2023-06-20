14aab8f5a0c9d96567896012922113e724e9791d
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-20 10:33:50 +0000

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ printf("Last digit of %d is 0 and is 0\n", n);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 }
 return (0);
