df539b62936cb3fcb8051de09c8b79975135d503
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-21 10:34:45 +0000

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ * Return: always ZERO
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
