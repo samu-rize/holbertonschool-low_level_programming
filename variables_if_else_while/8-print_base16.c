1c42309bd44184a38a3af844ec69df71386c9a8c
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-21 09:37:13 +0000

    8. Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - program prints the alphabet in lowercase, followed by a new line.
+ * main - program that prints all the numbers of base 16 in lowercase, followed by a new line.
  * Return: always ZERO
 **/
 int main(void)
