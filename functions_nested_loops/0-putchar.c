b026a5343ee53784657e74cff277f974ed5f2147
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-21 11:17:41 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Write a program that prints _putchar, followed by a new line.
  * Return: always ZERO
  */
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	char ch[] = "_putchar";
+	int i;
+
+	for (i = 0; ch[i] != '\0'; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
