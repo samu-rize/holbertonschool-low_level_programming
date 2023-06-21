26022644398de9d5fefc2ca3298b98fc544fea76
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-21 12:24:47 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * Return: always ZERO
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
