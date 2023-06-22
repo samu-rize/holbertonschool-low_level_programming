fa36eec416bae703b55a43a43b6f86ce46671701
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-22 17:16:00 +0000

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,18 +5,19 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, r;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar ('0' + i * j);
+			r = i * j;
+			if (r > 9)
+				_putchar ('0' + r / 10);
+			_putchar ('0' + r % 10);
 			if (j == 9)
-			{
 				break;
-			}
-			
+
 			_putchar (',');
 			_putchar (' ');
 			_putchar (' ');
