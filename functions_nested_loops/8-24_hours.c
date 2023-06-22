f3814eecfee0af2671f0d2c461d4cb65fb6a0c65
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-22 16:35:27 +0000

    clock

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 					_putchar ('0' + m1);
 					_putchar ('0' + m);
 					_putchar ('\n');
-					if (h1 == 2 && h == 3 && m1 == 5 && m == 9)
+					if (h1 == 0 && h == 0 && m1 == 2 && m == 9)
 						b = 1;
 					if (b == 1)
 						break;
