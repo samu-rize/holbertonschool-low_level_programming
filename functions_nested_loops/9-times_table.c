f38c082b6b231b5c6d37f58c5fdbf8640ae51577
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-22 17:08:47 +0000

    times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - check the code
+ * Return: always ZERO
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar ('0' + i * j);
+			_putchar (',');
+			_putchar (' ');
+			_putchar (' ');
+		}
+		_putchar ('\n');
+	}
+
+}
