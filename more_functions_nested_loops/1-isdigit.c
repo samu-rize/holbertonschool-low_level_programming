49b10cf9283ff4bc3b831e07111f8ee633504e79
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-23 18:08:07 +0000

    this is the task number 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check the code
+ * @c: integer
+ * Return: always ZERO
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
