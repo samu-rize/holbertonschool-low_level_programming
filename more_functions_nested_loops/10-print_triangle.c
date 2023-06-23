cec1cde37af1504271a5964fcdd923930ec60d2e
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-23 20:44:37 +0000

    this is the task number 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - check the code
+ * @s: integer
+ * Return: always ZERO
+ */
+void print_triangle(int s)
+{
+	int i, j;
+
+	for (i = s; i > 0; i--)
+	{
+		for (j = 0; j < s; j++)
+		{
+			if (j < i - 1)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
