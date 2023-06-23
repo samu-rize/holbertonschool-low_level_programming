f8e4326e7e1fed8842e7d43cc54c50fa23afb672
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-23 20:19:22 +0000

    this is the task number 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 101; ++i)
+	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -23,11 +23,15 @@ int main(void)
 		}
 		else
 		{
-			putchar(i);
+			printf("%d", i);
 		}
 		if (i < 100)
 		{
 			putchar(' ');
+		}
+		else
+		{
+			putchar('\n');
 		}
 	}
 	return (0);
