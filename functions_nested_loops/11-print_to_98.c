3ffdf4678c8394d74b3cf4080ce3c0d0a8b8dfe7
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-23 09:03:40 +0000

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_to_98 - check the code
+ * @n: integer
+ * Return: always ZERO
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i < 99; i++)
+		{
+			if (i > 9)
+			{
+				_putchar ('0' + i / 10);
+				_putchar ('0' + i % 10);
+			}
+			else
+			{
+				_putchar ('0' + i);
+			}
+			if (i != 98)
+			{
+				_putchar (',');
+				_putchar (' ');
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		_putchar ('9');
+		_putchar ('8');
+	}
+	else
+	{
+		for (i = n; i > 97; i++)
+		{
+			_putchar ('0' + i / 10);
+			_putchar ('0' + i % 10);
+				if (i != 98)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+		}
+	}
+}
