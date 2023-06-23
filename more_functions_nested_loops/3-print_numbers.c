86c5204b3b7f0a7a66dcd40164687f95ad86b505
samu-rize <135615520+samu-rize@users.noreply.github.com>
2023-06-23 18:35:10 +0000

    this is the task number 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - check the code
+ * Return: always ZERO
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar ('0' + i);
+	}
+	_putchar ('\n');
+}
