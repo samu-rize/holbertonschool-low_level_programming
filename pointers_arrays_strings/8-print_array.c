4d6addc85ff5fc61fa377b02db64c1a6ca9553ca
samu-rize <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>
2023-06-29 15:11:08 +0200

    task number 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints a string in reverse
+ * @a: The input string
+ * @n: The input string
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf ("%d", a[i]);
+		if (i == n - 1)
+		{
+			break;
+		}
+		printf (", ");
+	}
+	
+	putchar ('\n');
+}
