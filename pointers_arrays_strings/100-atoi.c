646f91208acb8dc140ccc10d2792fd1ae25151a3
samu-rize <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>
2023-06-29 23:30:12 +0200

    task number 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,20 +1,44 @@
 #include "main.h"
+
+/**
+ * negative - calculate how many (-)
+ * in the given string
+ * @c: The input string
+ * Return: integer
+ */
+
+int negative(char *c)
+{
+	int i, neg = 0;
+
+	for (i = 0; c[i] != '0'; i++)
+	{
+		if (c[i] == '-')
+			neg += 1;
+	}
+	if (neg % 2)
+		neg = 1;
+	else
+		neg = -1;
+	return (neg);
+}
+
 /**
  * _atoi - Prints a string in reverse
  * @s: The input string
  * Return: char
  */
+
 int _atoi(char *s)
 {
-	int i, rsl, sign;
+	int i, rsl, neg;
 
+	rsl = 0;
+	neg = negative(s);
 	for (i = 0; s[i] != '\0'; i++)
 	{	
-		if (s[i] == '-')
-			sign = -1;
-		else
-			sign = 1;
-		rsl = rsl * 10 + s[i] -'0';
+		if (s[i] >= '0' && s[i] <= '9')
+			rsl *= 10 + s[i] -'0';
 	}
-	return (rsl * sign);
+	return (rsl * neg);
 }
