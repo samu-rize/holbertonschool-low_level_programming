803a1bf7059ae77790b35c00afa4ffc1ebf17b42
samu-rize <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>
2023-06-29 15:12:10 +0200

    task number 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,15 +9,16 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
-		printf ("%d", a[i]);
+		printf("%d", a[i]);
 		if (i == n - 1)
 		{
 			break;
 		}
-		printf (", ");
+		printf(", ");
 	}
 	
-	putchar ('\n');
+	putchar('\n');
 }
