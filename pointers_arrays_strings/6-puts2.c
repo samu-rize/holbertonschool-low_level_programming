be273acc08b819ef9b2fda61e65187e08006d4cf
samu-rize <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>
2023-06-29 03:14:07 +0200

    task number 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - Prints a string in reverse
+ * @str: The input string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i, j;
+	j = strlen (str);
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar (str[i]);
+	}
+	_putchar ('\n');
+}
