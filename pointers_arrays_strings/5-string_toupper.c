37606611b11f8080a2ee4f34903a985a4b4a546d
samu-rize <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>
2023-06-30 22:28:03 +0200

    task number 4

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,15 @@
  */
 char *string_toupper(char *str)
 {
-    char *ptr = str;
-    while (*str != '\0')
-    {
-        if (*str >= 'a' && *str <= 'z')
-        {
-            *str = *str - 'a' + 'A';
-        }
-        str++;
-    }
-    return (ptr);
+	char *ptr = str;
+
+	while (*str != '\0')
+	{
+		if (*str >= 'a' && *str <= 'z')
+		{
+			*str = *str - 'a' + 'A';
+		}
+		str++;
+	}
+	return (ptr);
 }
