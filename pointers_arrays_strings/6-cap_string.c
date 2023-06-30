b8b3adfbb03d757dccf817f971cc454320579f05
samu-rize <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>
2023-06-30 22:46:12 +0200

    task number 4

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: array of chars
+ *
+ * Return: array
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+
+	while (*str != '\0')
+	{
+        if (*str == ' ')
+        {
+            str++;
+        }
+        
+		if (*str >= 'a' && *str <= 'z')
+		{
+			*str = *str - 'a' + 'A';
+		}
+		str++;
+	}
+	return (ptr);
+}
