4cf8d020a206e16d67346341bcfa89824db56205
samu-rize <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>
2023-06-30 23:55:56 +0200

    task number 4

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
 char *cap_string(char *str)
 {
 	char *ptr = str;
-	int i = 0;
+	int i = 1;
 
 	while (*str != '\0')
 	{
@@ -19,6 +19,9 @@ char *cap_string(char *str)
 		if (*str >= 'a' && *str <= 'z' && i == 1)
 		{
 			*str = *str - 'a' + 'A';
+		}
+		if (*str >= 'A' && *str <= 'Z' && i == 1)
+		{
 			i = 0;
 		}
 		str++;
