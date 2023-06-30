572de1d29db8a3690fb299201af5c6bb4d9d45eb
khbesbes <5786@holbertonstudents.com>
2023-06-30 11:25:06 +0100

    finelly :')

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,71 +1,24 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: char
- * Return: int
- */
+ * _atoi - string to an integer
+ * @s: array of chars
+ * Return: integer
+*/
 
 int _atoi(char *s)
 {
-	int rsl = 0, i, sign = 1;
+	unsigned int rsl = 0, sign = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			rsl = rsl * 10 + s[i] - '0';
-		}
+		if (*s >= '0' && *s <= '9')
+			rsl = (rsl * 10) + (*s - '0');
+		else if (*s == 45)
+			sign = sign * -1;
+		else if (rsl > 0)
+			break;
+		s++;
 	}
 	return (rsl * sign);
 }
-
-/**
- * negative - calculate how many (-)
- * in the given string
- * @c: The input string
- * Return: integer
- 
-
-int negative(char *c)
-{
-	int i, neg = 0;
-
-	for (i = 0; c[i] != '\0'; i++)
-	{
-		if (c[i] == '-')
-			neg++;
-	}
-	if (neg % 2 != 0)
-		neg = -1;
-	else
-		neg = 1;
-	return (neg);
-}
-*/
-/**
- * _atoi - Prints a string in reverse
- * @s: The input string
- * Return: char
- 
-
-int _atoi(char *s)
-{
-	int i, rsl = 0, neg = negative(s);
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-			rsl = rsl * 10 + (s[i] - '0');
-		else if (rsl > 0)
-		{
-			break;
-		}
-	}
-	return (rsl * neg);
-}
-*/
