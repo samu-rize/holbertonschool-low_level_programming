7ce5e61a68cb3cbbfbef9be50eff845c3a68e6c5
samu-rize <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>
2023-07-01 18:32:21 +0200

    task number 0

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - prints buffer in hexa
+ * @s: a pointer to the memory area to be filled.
+ * @b: the constant byte to fill the memory with.
+ * @n: the number of bytes to be filled.
+ *
+ * Return: char.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
