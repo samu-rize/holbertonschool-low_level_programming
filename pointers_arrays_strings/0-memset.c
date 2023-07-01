9137351f1ee1db61a0f6817af982ae912c2e0dcd
samu-rize <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>
2023-07-01 17:42:39 +0200

    task number 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -21,17 +21,6 @@ char *_memset(char *s, char b, unsigned int n)
 		{
 			s[i] = b;
 		}
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", s[i]);
-		i++;
 	}
-	printf("\n");
 	return (s);
 }
