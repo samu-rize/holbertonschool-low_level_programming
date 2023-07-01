5aa250ac623d7119fba84254d62a46844027719b
samu-rize <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>
2023-07-01 22:53:45 +0200

    task number 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - concatenates two strings.
+ * @dest: array of chars
+ * @src: array of chars
+ * @n: integer
+ * Return: Always 0.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned short int i, j, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+			}
+			
+		}
+		
+	}
+	
+}
