fe49d3ac0562fe9fa2ce04298442cfb36bc3d141
samu-rize <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>
2023-07-02 00:12:16 +0200

    task number 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,9 +3,8 @@
 
 /**
  * _strspn - concatenates two strings.
- * @dest: array of chars
- * @src: array of chars
- * @n: integer
+ * @s: array of chars
+ * @accept: array of chars
  * Return: Always 0.
  */
 
@@ -15,6 +14,10 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')))
+		{
+			break;
+		}
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
