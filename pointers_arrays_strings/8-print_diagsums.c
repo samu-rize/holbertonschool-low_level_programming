53b16d461ecc926a61997a45977ccb44a9ee92e3
samu-rize <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>
2023-07-02 19:36:56 +0200

    task number 7

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  prints the chessboard.
+ * @a: array of chars representing the chessboard.
+ * @s: size of the array.
+ * Return: Always 0.
+ */
+
+void print_diagsums(int *a, int s)
+{
+	int i, j = s - 1, n = 0, m = 0;
+
+	for (i = 0; i < s; i++)
+	{
+		n += *(a + i * s + i);
+	}
+	for (i = 0; i < s && j > 0; i++, j--)
+	{
+		m += *(a + i * s + j);
+	}
+}
