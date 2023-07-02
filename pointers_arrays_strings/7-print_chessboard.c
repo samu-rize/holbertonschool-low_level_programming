86038aa898c143416e28997d8371be2b59ef378f
samu-rize <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>
2023-07-02 17:41:55 +0200

    task number 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_chessboard -  locates a substring.
- * @a: array of chars
+ * print_chessboard -  prints the chessboard.
+ * @a: array of chars representing the chessboard.
  * Return: Always 0.
  */
 
@@ -11,17 +11,16 @@ void print_chessboard(char (*a)[8])
 {
 	unsigned short int i, j;
 
-	for (i = 0; a[i] != '\0'; i++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; a[i][j] != '\0'; j++)
 		{
-            if ((a[i][j] >= 'A' && a[i][j] <= 'Z') ||
-            (a[i][j] >= 'a' && a[i][j] <= 'z') || (a[i][j] == ' '))
-            {
-                putchar (a[i][j]);
-            }
+			if ((a[i][j] >= 'A' && a[i][j] <= 'Z') ||
+			(a[i][j] >= 'a' && a[i][j] <= 'z') || (a[i][j] == ' '))
+			{
+				putchar (a[i][j]);
+			}
 		}
-        putchar ('\n');
+		putchar ('\n');
 	}
-	return (NULL);
 }
