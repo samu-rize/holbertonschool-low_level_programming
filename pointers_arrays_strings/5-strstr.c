01927d9379114f5c5ac9d80d2f5de806704a41f6
samu-rize <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>
2023-07-02 14:45:15 +0200

    task number 5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,15 +10,26 @@
 
 char *_strstr(char *s, char *a)
 {
-	unsigned short int i, j;
+	unsigned short int i, j, c, n;
 
+	n = 0;
+	for (c = 0; a[c] != '\0'; c++)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == a[j])
+			if (s[i] != a[j])
 			{
-				return (&s[i]);
+				n = 0;
+			}
+			else
+			{
+				n++;
+				i++;
+				if (n == c)
+				{
+					return(&s[i - n]);
+				}
 			}
 		}
 	}
