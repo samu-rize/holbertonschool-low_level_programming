92cdc448c59fcc8b397e1d4c65f93ba654c2c623
khbesbes <5786@holbertonstudents.com>
2023-07-03 18:30:15 +0100

    correct the comment

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * _puts_recursion - writes the character c to stdout
- *@s: klfdo
- * Return: .
+ * _puts_recursion - prints a string followed by a newline character
+ * @s: The string to be printed
+ *
+ * Return: void
  */
 
 void _puts_recursion(char *s)
