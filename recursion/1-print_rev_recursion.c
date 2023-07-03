a6f2fd6172bc36a06fe8033ef4934c82704dafe0
khbesbes <5786@holbertonstudents.com>
2023-07-03 18:33:49 +0100

    create 1-print_rev_recursion.c task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse using recursion
+ * @s: The string to print in reverse
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	for (; s[i] >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
