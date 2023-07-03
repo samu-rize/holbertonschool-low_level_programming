59f0cb7abf1de292576e0eae5a7ad083886f094a
khbesbes <5786@holbertonstudents.com>
2023-07-03 19:06:36 +0100

    fix errors in 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,4 +1,24 @@
 #include "main.h"
+#include <string.h>
+
+/**
+ * print - recursively prints a string in reverse
+ * @s: The string to be printed
+ * @i: The index of the character to be printed
+ *
+ * Return: void
+ */
+
+void print(char *s, int i)
+{
+	_putchar(s[i]);
+	i--;
+	if (i >= 0)
+	{
+		print(s, i);
+	}
+}
+
 /**
  * _print_rev_recursion - prints a string in reverse using recursion
  * @s: The string to print in reverse
@@ -10,10 +30,6 @@ void _print_rev_recursion(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{}
-	for (; s[i] >= 0; i--)
-	{
-		_putchar(s[i]);
-	}
+	i = strlen(s);
+	print(s, i);
 }
