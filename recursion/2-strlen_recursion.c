7c0f320f1eee5308e5fd9bdb874e0c9fbb47ab2c
khbesbes <5786@holbertonstudents.com>
2023-07-04 13:07:44 +0100

    fix betty

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,13 +5,14 @@
  * _strlen_recursion - computes the length of a string using recursion
  * @s: The string to compute the length of
  *
- * This function recursively calculates the length of a string by incrementing
- * a counter variable 'i' each time it makes a recursive call. It takes a string
- * 's' as a parameter. If the character at the current position is not the null
- * terminator ('\0'), the function adds 1 to the counter and makes a recursive
- * call by moving the string pointer to the next character. The recursive calls
- * continue until the null terminator is encountered, at which point the function
- * returns the final count.
+ * This function recursively calculates the length of a string by
+ * incrementing a counter variable 'i' each time it makes a recursive
+ * call. It takes a string 's' as a parameter. If the character at the
+ * current position is not the null terminator ('\0'), the function
+ * adds 1 to the counter and makes a recursive call by moving the string
+ * pointer to the next character. The recursive calls continue until
+ * the null terminator is encountered, at which point the function returns
+ * the final count.
  *
  * Return: The length of the string
  */
