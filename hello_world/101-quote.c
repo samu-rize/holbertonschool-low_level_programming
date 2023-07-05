b9498e32423c888800731263b952f87cd4d66c93
samu-rize <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>
2023-07-05 22:57:52 +0200

    create the code

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a string.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    printf ("that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
