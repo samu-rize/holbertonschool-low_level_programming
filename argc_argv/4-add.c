cc3d269112abb70c62247f08fec02879bb2cf846
khbesbes <5786@holbertonstudents.com>
2023-07-07 12:41:14 +0100

    create the code

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: The number of command-line arguments passed to the program.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(int argc, char **argv)
+{
+	int i, s = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argv[i] < '0' || argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		s += atoi(argv[i]);
+	}
+	printf("%d\n", s);
+	return (0);
+}
