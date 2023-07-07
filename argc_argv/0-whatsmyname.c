794c859148eb4c902e6eda47cad3661ea52d16ff
khbesbes <5786@holbertonstudents.com>
2023-07-07 10:17:51 +0100

    fix the betty error

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-/*
-     * Function: main
-     * -------------------
-     * The main entry point of the program.
-     *
-     * argc: The number of command-line arguments passed to the program.
-     * argv: An array of strings representing the command-line arguments.
-     *
-     * returns: An integer value indicating the exit status of the program.
-     */
+/**
+ * main - Entry point of the program
+ *
+ * argc: The number of command-line arguments passed to the program.
+ * argv: An array of strings representing the command-line arguments.
+ *
+ * returns: An integer value indicating the exit status of the program.
+ */
 
 int main(int __attribute__ ((unused)) argc, char **argv)
 {
