95c0b15e40fee119545318ed9331bbda9b3c2cea
khbesbes <5786@holbertonstudents.com>
2023-07-11 17:02:23 +0100

    create the code

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,20 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * create_array - Creates an array of characters dynamically with the given
- * size and initializes
- * each element with the specified character.
- *
+ * create_array - Creates an array of characters dynamically with the given size and initializes
+ *                each element with the specified character.
  * @size: The size of the array to be created.
  * @c: The character to initialize each element of the array with.
  *
- * Return:
- *    - A pointer to the dynamically allocated array if successful.
- *    - NULL if size is 0 or if memory allocation fails.
+ * Return: A pointer to the dynamically allocated array if successful, otherwise NULL.
+ *
+ * Description:
+ *  - This function dynamically allocates memory for an array of characters with the given size.
+ *  - If the size is 0, it returns NULL indicating an invalid size.
+ *  - The function then initializes each element of the array with the specified character 'c'.
+ *  - The allocated memory is returned to the caller.
+ *  - It's the caller's responsibility to free the allocated memory using free() when done.
  */
-
 
 char *create_array(unsigned int size, char c)
 {
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Creates an array of characters dynamically with the given size and initializes
+ *                each element with the specified character.
+ * @str: The size of the array to be created.
+ *
+ * Return: A pointer to the dynamically allocated array if successful, otherwise NULL.
+ *
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, l;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		l= strlen(str);
+		ptr = (char *)malloc((l + 1) * sizeof(char));
+		if (ptr == 0)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < l; i++)
+			{
+				ptr[i] = str [i];
+			}
+			ptr[i] = '\0';
+			return (ptr);
+			free(ptr);
+		}
+	}
+}
