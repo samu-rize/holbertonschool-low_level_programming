eef40c6bf64c1809669ce47da26a8498a557109b
khbesbes <5786@holbertonstudents.com>
2023-07-12 10:50:29 +0100

    2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Creates an array of characters dynamically with
+ * the given size and initializes
+ *                each element with the specified character.
+ * @s1: the first string.
+ * @s2: the second string.
+ *
+ * Return: A string that contain the first and second strings, otherwise NULL.
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned int i, ls1, ls2;
+
+	if ((s1 == NULL) & (s2 == NULL))
+	{
+		return (NULL);
+	}
+	else if (s1 == NULL)
+	{
+		return (s2);
+	}
+	else if (s2 == NULL)
+	{
+		return (s1);
+	}
+		ls1 = strlen(s1);
+		ls2 = strlen(s2);
+		ptr = (char *)malloc((ls1 + ls2 + 1) * sizeof(char));
+		if (ptr == 0)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; i < (ls1 + ls2); i++)
+			{
+				if (i < ls1)
+				{
+					ptr[i] = s1[i];
+				}
+				else
+				{
+					ptr[i] = s2[i - ls1];
+				}
+			}
+			ptr[i] = '\0';
+			return (ptr);
+			free(ptr);
+		}
+	}
+
