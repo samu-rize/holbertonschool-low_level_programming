b1f728724cc676985a62b60716a0b211160a3a89
khbesbes <5786@holbertonstudents.com>
2023-07-14 12:19:09 +0100

    fix errors

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	if (s1 == '\0')
+	if (*s1 == '\0')
 	{
 		s1 = "";
 	}
-	if (s2 == '\0')
+	if (*s2 == '\0')
 	{
 		s2 = "";
 	}
