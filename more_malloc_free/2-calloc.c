d1f2f9862416c449b46d37433f157b9ef59ad1b6
khbesbes <5786@holbertonstudents.com>
2023-07-14 13:55:47 +0100

    fix betty style

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 /**
- * _calloc - Allocates memory for an array using malloc and initializes it to 0.
+ * _calloc - Allocates memory for an array using malloc and
+ * initializes it to 0.
  * @nmemb: The number of elements in the array.
  * @size: The size of each element.
  *
@@ -25,7 +26,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	memset (ptr, 0, nmemb * size);
+	memset(ptr, 0, nmemb * size);
 	return (ptr);
 	free(ptr);
 }
