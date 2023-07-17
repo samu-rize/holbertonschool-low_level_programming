170ca7f02de8cbe8a6a97f6b2a2171aaa72f757a
samu-rize <6710@holbertonstudents.com>
2023-07-17 16:38:36 +0100

    fix the error

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,13 @@
-#include <dog.h>
+#include "dog.h"
+
+/**
+ * struct dog - Struct to hold dog information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This struct is used to store information about dogs.
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
