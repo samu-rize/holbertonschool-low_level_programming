9f51a8b47cbf2a55130ba869cef492043d4f5b31
samu-rize <6710@holbertonstudents.com>
2023-07-18 11:27:23 +0100

    fix comment block

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,17 +1,12 @@
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog with given information
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * free_dog - Frees the memory allocated for a dog
+ * @d: Pointer to the dog to be freed
  *
- * Description: This function creates a new dog with the specified name, age,
- *              and owner. It allocates memory for the dog structure and the
- *              copies of the name and owner strings. Returns a pointer to the
- *              newly created dog, or NULL if memory allocation fails.
- * Return: Pointer to the newly created dog, or NULL if memory allocation
- * fails.
+ * Description: This function frees the memory allocated for a dog structure.
+ * 
+ * Return: None
  */
 
 void free_dog(dog_t *d);
