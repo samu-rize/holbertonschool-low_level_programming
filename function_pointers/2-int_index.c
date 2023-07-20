8ec5e67976038a56eaf9c77fa3e08c067630ddc1
samu-rize <6710@holbertonstudents.com>
2023-07-20 11:49:56 +0100

    fix the comment block and the errors

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,27 +1,31 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Iterates through an array and performs an action
- * on each element.
+ * int_index - Searches for an integer in an array.
  * @array: Pointer to the array.
  * @size: The size of the array.
- * @action: A pointer to the function used to perform the action
- * on each element.
+ * @cmp: A pointer to the function used for comparison.
  *
- * Description: This function takes an array, its size, and a function pointer
- * as arguments.
- * If the array is NULL, the size is 0, or the function pointer is NULL,
- * the function does nothing. Otherwise, it iterates through the array
- * and calls the function pointed by 'action' to perform an action
- * on each element.
+ * Description: This function searches for an integer in the array by using
+ * the comparison function 'cmp'. If the array is NULL, the size
+ * is 0, or the comparison function is NULL, the function returns -1.
+ * Otherwise, it iterates through the array and calls the 'cmp' function
+ * on each element until a match is found. If a match is found, the
+ * index of the matching element is returned.
+ *
+ * Return: The index of the first element for which the 'cmp' function
+ * does not return 0.
+ * If no such element is found, or if any input is invalid, the function
+ * returns -1.
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (!array || size == 0 || !cmp)
 	{
-		return
+		return;
 	}
 
 	for (i = 0; i < size; i++)
