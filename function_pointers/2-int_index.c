88b7069be49c7bb45ff975fe01a38e7ece95c3d8
samu-rize <6710@holbertonstudents.com>
2023-07-20 11:53:33 +0100

    fix the errors

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -23,13 +23,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (!array || size == 0 || !cmp)
+	if (!array || size <= 0 || !cmp)
 	{
-		return;
+		return(-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
 		cmp(array[i]);
 	}
+	return(-1);
 }
