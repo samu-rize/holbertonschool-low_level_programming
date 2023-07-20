4ea3cf1bba97ca9eaa6635a271dca3bdd003b1d1
samu-rize <6710@holbertonstudents.com>
2023-07-20 11:58:24 +0100

    fix the errors

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -25,12 +25,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (!array || size <= 0 || !cmp)
 	{
-		return(-1);
+		return (-1);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
 	}
-	return(-1);
+	return (-1);
 }
