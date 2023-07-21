04b6ad66c608b552cb744bae8fc04b1e3a257c9d
samu-rize <6710@holbertonstudents.com>
2023-07-21 13:01:36 +0100

    fix betty warnings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,26 +1,26 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - Prints a variable number of integers.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers to print.
- * @...: Variable arguments representing the integers to be printed.
+ * print_strings - Prints a variable number of strings.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings to print.
+ * @...: Variable arguments representing the strings to be printed.
  *
  * Description: This function takes a separator string, the number
- * of integers 'n', and a variable number of arguments representing
- * the integers to be printed.
- * It prints the integers, separated by the 'separator' string.
+ * of strings 'n', and a variable number of arguments representing
+ * the strings to be printed. It prints the strings, separated by the
+ * 'separator' string. If a string is NULL, "(nil)" is printed in its place.
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list arg;
-    unsigned int i;
+	va_list arg;
+	unsigned int i;
 	char *s;
 
-    va_start(arg, n);
+	va_start(arg, n);
 
-    for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s = va_arg(arg, char *);
 		if (!s)
