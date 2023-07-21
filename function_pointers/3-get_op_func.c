b407b03c150bee2ac190b82b94d4d859ab44c06b
samu-rize <6710@holbertonstudents.com>
2023-07-21 16:29:22 +0100

    create get op func

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - Selects the appropriate function to perform the operation.
+ * @s: The operator passed as an argument.
+ *
+ * Description: This function takes an operator as a string and selects the
+ *              appropriate function to perform the operation based on the
+ *              operator. It returns a pointer to the corresponding function.
+ *              If the operator is not recognized, it returns NULL.
+ *
+ * Return: A pointer to the appropriate function that performs the operation.
+ *         If the operator is not recognized, it returns NULL.
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	unsigned int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	for (i = 0; (ops + i)->op != NULL ; i++)
+	{
+		if (strcmp(s, (ops + i)->op) == 0)
+		{
+			return ((ops + i)->f);
+		}
+	}
+	return (NULL);
+}
