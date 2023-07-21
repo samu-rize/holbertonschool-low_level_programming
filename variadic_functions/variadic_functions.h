313cb609f6949bb499f835659249c10abdc6cf68
samu-rize <6710@holbertonstudents.com>
2023-07-21 10:56:00 +0100

    add print_numbers

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,10 @@
 #ifndef FP_H
 #define FP_H
 
+#include <stdio.h>
 #include <stdarg.h>
- 
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
