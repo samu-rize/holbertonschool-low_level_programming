ac3c2ae37b267a7c77caf20b37ab4b655f365a89
samu-rize <6710@holbertonstudents.com>
2023-07-21 10:23:04 +0100

    fix betty warnings

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,13 +17,15 @@ int sum_them_all(const unsigned int n, ...)
 	va_list arg;
 	unsigned int i;
 	int sum = 0;
-	va_start (arg, n);
+
+	va_start(arg, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg (arg, int);
+		sum += va_arg(arg, int);
 	}
 
-	va_end (arg);
+	va_end(arg);
+
 	return (sum);
 }
