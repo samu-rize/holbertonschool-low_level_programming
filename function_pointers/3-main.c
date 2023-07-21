8866ea326e83be3169240c861d9527049bd322fe
samu-rize <6710@holbertonstudents.com>
2023-07-21 16:53:40 +0100

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,43 +1,44 @@
+#include<stdio.h>
 #include "3-calc.h"
-
+#include<stdlib.h>
+int (*get_op_func(char *s))(int, int);
 /**
- * main - Entry point for the calculator program.
- * @argc: The number of command-line arguments.
- * @argv: An array of pointers to the command-line arguments.
- *
- * Return: Always 0 (Success).
+ * main - a program that performs simple operations
+ * @argc: argiment count
+ * @argv: argument value
+ * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int);
-	int num1, num2, result;
+	int a, b;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
 
-	/** if ((argc != 4) || argv[3] == NULL)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
-	}**/ 
-
-	op_func = get_op_func(argv[2]);
-
-	if (!op_func)
-	{
-		printf("Error\n");
-		return (99);
+		exit(98);
 	}
-
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+			&& argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
-		return (100);
+		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-	result = op_func(num1, num2);
-	printf("%d\n", result);
 
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+
+	}
+/*	if (get_op_func(argv[2]) == NULL)  */
+/*	{*/
+	/*	printf("Error\n");*/
+	/*	exit(99);*/
+/*	}*/
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }
