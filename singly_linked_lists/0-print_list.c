609d4d35969ca13a069d51470064a70e8a415a78
samu-rize <6710@holbertonstudents.com>
2023-08-02 20:19:39 +0100

    create print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_list - function that print all elements of a list
+ *
+ * @h: the varible with type list_t
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
