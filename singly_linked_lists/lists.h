fbd4c3a9c34cfa9b1cce4c682c323473e76cc914
samu-rize <6710@holbertonstudents.com>
2023-08-02 20:27:57 +0100

    fix betty errors

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,12 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
 typedef struct list_s
 {
-    char *str;
-    unsigned short int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned short int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 
