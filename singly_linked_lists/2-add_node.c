eb41f6db9f7d175262a76307e411cedf5c708cc5
samu-rize <6710@holbertonstudents.com>
2023-08-03 16:40:39 +0100

    fix betty errors

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,28 +7,29 @@
  * Return: A pointer to the newly created node. If memory allocation fails,
  *         the function returns NULL.
  */
-list_t *add_node(list_t **head, const char *str) {
-
-    list_t *new;
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
 	int i;
 
 	if (str == NULL)
-        return (NULL);
+		return (NULL);
 
 	new = (list_t *)malloc(sizeof(list_t));
 
-    if (new == NULL)
+	if (new == NULL)
 	{
-        return (NULL);
+		return (NULL);
 	}
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+	;
 
-    new->str = strdup(str);
+	new->str = strdup(str);
 	new->len = i;
-    new->next = *head;
+	new->next = *head;
 
-    *head = new;
+	*head = new;
 
-    return (new);
+	return (new);
 }
