beaba65e544c0b5e643eacaa368d3a9fba34ccce
samu-rize <6710@holbertonstudents.com>
2023-08-28 16:09:40 +0100

    fix betty

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,12 +4,14 @@
  * get_bit - gets the bit at the index
  * @n: integer
  * @index: bit
+ *
+ * Return: int
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(n) * 8)
-    {
-        return (-1);
-    }
-    return (n >> index & 1);
+	if (index >= sizeof(n) * 8)
+	{
+		return (-1);
+	}
+	return (n >> index & 1);
 }
