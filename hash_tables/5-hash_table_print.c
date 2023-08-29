d534b2d5860c04438a03ca64950927c5e8d05e9e
samu-rize <6710@holbertonstudents.com>
2023-08-29 17:18:36 +0100

    add hash table print

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print -function that prints a hash table
+ * @ht: the hash table
+ * Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int f_comma = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *node = ht->array[i];
+
+		while (node != NULL)
+		{
+			if (!f_comma)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			f_comma = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
