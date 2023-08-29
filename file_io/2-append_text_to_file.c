61f1b4204d8e736e649200cc1a99f8f28c8ad7e0
samu-rize <6710@holbertonstudents.com>
2023-08-29 15:27:45 +0100

    fix problem

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
 /**
- * create_file - function that creates a file.
+ * append_text_to_file - function that append text to file.
  * @filename: file path
  * @text_content: text content
  * Return: success (1)
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	size_t byte;
 	int bw, file_fd;
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file_fd = open(filename, O_CREAT | O_WRONLY | O_APPEND, 0600);
 	if (text_content != NULL)
 	{
 		byte = strlen(text_content);
