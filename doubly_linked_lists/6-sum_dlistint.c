03ce31e959364236ad6e5fe52dc4fd6f74cec3dc
samu-rize <6710@holbertonstudents.com>
2023-08-30 13:20:27 +0100

    add sum distint

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -10,14 +10,14 @@ int sum_dlistint(dlistint_t *head)
 	int sum = 0;
 
 	if (head == NULL)
-		{
-			return (0);
-		}
+	{
+		return (0);
+	}
 	while (head)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	
-	return sum;
+
+	return (sum);
 }
